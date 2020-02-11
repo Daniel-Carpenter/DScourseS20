6af17e5e5558785b8cdfd909403de6cf49e61310
Daniel-Carpenter <dbry1024@gmail.com>
2020-02-11 16:03:23 -0600

    Completing PS5

--- ProblemSets/PS5/PS5_Carpenter.R
+++ ProblemSets/PS5/PS5_Carpenter.R
@@ -1,5 +1,57 @@
 library(tidyverse)
-#library(rtsdata)
+library(rvest)
+library(httr)
 
-#df <- ds.getSymbol.yahoo('AAPL', from = "1900-01-01", to = Sys.Date())
-table(df)
+## INPUTS
+  link <- "https://www.macrotrends.net/2324/sp-500-historical-chart-data"
+  selector <- "#style-1 > table"
+
+## Link to table in webpage
+  list <- read_html(link)
+  list %>%
+    html_nodes(selector) %>%
+    html_table(fill=TRUE) 
+
+## Find Class Type
+  table <-
+    list %>%
+    html_nodes(selector) %>%
+    html_table(fill=TRUE) 
+  class(table)
+
+## List to data_frame
+  table <- 
+    table %>%
+    bind_rows() %>%
+    as_tibble()
+  table
+  
+#################################
+
+## Pull from Yahoo Finance
+  
+library(rtsdata)
+library(dplyr)
+
+startDate <- "1980-01-01"
+  
+AAPL  <- ds.getSymbol.yahoo('AAPL', from = startDate, to = Sys.Date())
+SnP   <- ds.getSymbol.yahoo('^GSPC', from = startDate, to = Sys.Date())
+GOOG   <- ds.getSymbol.yahoo('GOOG', from = startDate, to = Sys.Date())
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
