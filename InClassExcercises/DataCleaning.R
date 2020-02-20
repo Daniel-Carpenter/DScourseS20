620d5ed23f840e03e7eccd99ce6178ee5e3469a7
Daniel Carpenter <dbry1024@gmail.com>
2020-02-20 14:44:55 -0600

    Update DataCleaning.R

--- InClassExcercises/DataCleaning.R
+++ InClassExcercises/DataCleaning.R
@@ -1,5 +1,13 @@
 library(skimr)
 library(tidyverse)
-library()
+library(huxtable)
 
 df <- as_tibble(mtcars)
+skim(df)
+
+cor(df$mpg,df$wt)
+
+exp <- lm(log(mpg) ~ wt,data=df)
+tidy(exp)
+
+df1 %>% group_by(gear) %>% summarize()
