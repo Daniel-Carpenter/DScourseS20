61f249a247c5812cac8de5db848c319239c6343e
Daniel Carpenter <dbry1024@gmail.com>
2020-04-11 18:48:48 -0500

    Finished, need to Fix Optimization

--- FinalProject/Analysis_Carpenter/Analysis/PortfolioOptimizationV13 (newDataPull).R
+++ FinalProject/Analysis_Carpenter/Analysis/PortfolioOptimizationV13 (newDataPull).R
@@ -6,32 +6,32 @@
   library(tidyr)            # Used to pivot data frame
   library(BatchGetSymbols)  # Used to Pull in Stock Data
   library(Rglpk)            # Used to optimize portfolio weights
+  library(stargazer)
   library(ggthemes)
+  library(xtable)  
 
 # INPUTS  -------------------------------------------------------------------------------------------------------
-    startDate    <- Sys.Date() - 365 * 5
-    endDate     <- Sys.Date()
+    startDate    <- Sys.Date() - 365 * 20
+    endDate     <- Sys.Date() - 365 * 10
     
     #df.SP500     <- GetSP500Stocks()
     #stockList   <- df.SP500$Tickers
-    stockList    <- c('JNJ', 'PG', 'JPM', 'FB', 'MSFT', 'AAPL','MMM', 
-                    'CL', 'ED', 'MCK', 'DE', 'PFE', 'AXP', 'WU', 'GLW')
+    stockList    <- c('JNJ', 'PG', 'JPM', 'MSFT', 'AAPL','MMM')
     riskFreeRate  <- .0160
-    desiredReturn       <- 0.08
+    desiredReturn       <- 0.10   # 10% = 0.10
     dollarsInvested     <- 10000
   
 # DATA PULL -----------------------------------------------------------------------------------------------------
   # Make Variables Monthly
     desiredReturn <- desiredReturn / 12
-    riskFreeRate  <- riskFreeRate / 12
-    
+
   # Pull Stock Data
     df <- BatchGetSymbols(tickers = stockList, 
                           first.date = startDate,
                           last.date = endDate, 
                           freq.data = 'monthly',
                           cache.folder = file.path(tempdir(), 'BGS_Cache'))
-    
+  
   # Mutate Data (Drop Cols and Rename)
     df <- as.data.frame(df$df.tickers) %>%
       select(ref.date,
@@ -51,41 +51,47 @@
     df.return <- t(data.matrix(df.return %>%
       select(-stockName)))
     
-  # Calculate Excess Returns
-    df <- df %>%
-      mutate(stockReturn = stockReturn - riskFreeRate) %>%
-      rename(excessReturn = stockReturn)
-    
     df <- pivot_wider(df, 
                       names_from = stockName, 
-                      values_from = excessReturn)
+                      values_from = stockReturn)
     
-  # Create Matrix for Var-Cov Matrix Calculation
-    df.matrix     <- data.matrix(df %>% select(-date)) # Converts df to Matrix
+    # Create Matrix for Var-Cov Matrix Calculation
+    df.matrix     <- data.matrix(df %>% 
+                                   select(-date) %>%
+                                   drop_na())
+    
+  # Calculate Excess Returns
+    df.excess <-  df.matrix[,1:NCOL(df.matrix)] - df.return[,1:NCOL(df.matrix)] # Stock Price - mean Stock Price
+
+  # Stock List to Data Frame
+    stockList <- as.data.frame(stockList) #Needs to be other object on original pull  
+    stockList <- t(df.matrix)
+    
+  
     
 # MAIN -----------------------------------------------------------------------------------------------------------
   # Create Variance-Covariance Matrix
-      VarCovMatrix  <- (t(df.matrix) %*% df.matrix) / length(df) # Creates Variance Covariance Matrix
+      VarCovMatrix  <- (t(df.excess) %*% df.excess) / NROW(stockList) # Creates Variance Covariance Matrix
   
   # Create Weights Table
       stockWeights <- c()
 
-      for (i in 1:length(stockList))
+      for (i in 1:NCOL(df.matrix))
       {
         stockWeights[i] <- 1
       }
       stockWeights              <- as.data.frame(stockWeights)
         colnames(stockWeights)  <- c("portfolioWeight")
-        rownames(stockWeights)  <- stockList
+        rownames(stockWeights)  <- rownames(stockList)
       
       stockWeights <- t(as.matrix(stockWeights))
       stockWeights1 <- stockWeights
       
   # Optimize Risk, Given desired Expected Return
-      #objectiveFun      <- sqrt(diag(VarCovMatrix)) 
-      tempVarCovMatVector <- sqrt(stockWeights1 %*% VarCovMatrix)
+      objectiveFun      <- sqrt(diag(VarCovMatrix)) 
+      #tempVarCovMatVector <- sqrt(stockWeights1 %*% VarCovMatrix)
       
-      objectiveFun      <- tempVarCovMatVector
+      #objectiveFun      <- tempVarCovMatVector
       constraintInputs  <- rbind(stockWeights, as.vector(df.return))
       direction         <- c("==", "==")
       constraintValues  <- c(1, desiredReturn)
@@ -99,24 +105,48 @@
       portfolioOptimal <- cbind(as.data.frame(optimalWeights$solution),
                                 as.data.frame(optimalWeights$solution) * dollarsInvested)
         colnames(portfolioOptimal)  <- c("Stock Weight", "Dollar Investment")
-        rownames(portfolioOptimal)  <- stockList
-      
+        rownames(portfolioOptimal)  <- rownames(stockList)
+
   # Calculate Risk of Porfolio
-      risk            <- sqrt((t(portfolioOptimal$`Stock Weight`) %*% VarCovMatrix) %*% portfolioOptimal$`Stock Weight`) # Standard Deviation (or volatility) of the portfolio, i.e. risk
+      risk            <- sqrt(12) * sqrt((t(portfolioOptimal$`Stock Weight`) %*% VarCovMatrix) %*% portfolioOptimal$`Stock Weight`) # Standard Deviation (or volatility) of the portfolio, i.e. risk
       
   # Calculate Expected Return of Portfolio
       expectedReturn  <- 12 * (df.return %*% portfolioOptimal$`Stock Weight`)
   
   # Calculate Sharpe Ratio for Portfolio
-      sharpeRatio     <- (expectedReturn - (riskFreeRate * 12)) / risk   # objective function <- goal is to maximize return per unit risk taken
+      sharpeRatio     <- (expectedReturn - riskFreeRate) / risk 
       
   # Summary Table
       summaryTable <- as.data.frame(rbind(risk,expectedReturn, sharpeRatio))
         colnames(summaryTable)  <- c("Value")
         rownames(summaryTable)  <- c("Risk", "Expected Return", "Sharpe Ratio")
         
+      portfolioOptimal <- portfolioOptimal %>% 
+                          mutate("Stock Name" = rownames(portfolioOptimal)) %>%
+                          select("Stock Name", "Stock Weight", "Dollar Investment") %>%
+                          filter(portfolioOptimal[1] > 0)
+        
       cat("Total Investment:",dollarsInvested, sep = " ")
       print(portfolioOptimal)
       print(summaryTable)
-  
-  
+      
+# VISUALIZATIONS --------------------------------------------------------------------------
+    # Optimized Portfolio Stats
+      xtable(summaryTable)
+      xtable(portfolioOptimal)
+    
+    # Monthly Returns by Stock  
+      ggplot(data = df.visual, aes(
+        x = date, 
+        y = stockReturn,
+        color = stockName)) +
+        geom_point() +
+        theme_minimal() +
+        labs(title = "Stock Returns",
+             subtitle = "",
+             caption = "Data from Yahoo Finance.",
+             tag = "Figure 1",
+             x = "Date",
+             y = "Monthly Return")
+
+      
